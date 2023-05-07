a7e5d05ecc81e02fd98e0ef50a96ac03c68194b6
maashh <58293726+maashhCS@users.noreply.github.com>
2023-05-07 02:07:18 +0200

    fixed github pages blocking http request

--- src/fetch_api.js
+++ src/fetch_api.js
@@ -1,7 +1,7 @@
 import { changeContent } from "./change_content";
 
 export const cityNotFound = document.querySelector('.search-input-field-error-city-not-found');
-const apiUrl = 'http://api.weatherapi.com/v1/';
+const apiUrl = 'https://api.weatherapi.com/v1/';
 const apiKey = 'd7ad6837fd37407e8e0163826230105';
 
 export async function fetchApiData(cityName){
